970cbf4e93fe543d527da903f76b0990f469f77c
Puneet <puneet.maratha06@gmail.com>
2023-07-25 22:02:59 +0530

    text change 3

--- GitDemoApp/ContentView.swift
+++ GitDemoApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Git Demo Ji!")
+            Text("Hello, World!")
         }
         .padding()
     }
