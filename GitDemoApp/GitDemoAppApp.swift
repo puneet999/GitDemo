b3edd4170fc3e18d0e95ad3b02d67c56c98c9376
Puneet <>
2023-07-25 19:59:18 +0530

    First Commit

---
+++ GitDemoApp/GitDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoAppApp.swift
+//  GitDemoApp
+//
+//  Created by Puneet on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
